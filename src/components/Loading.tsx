f3b59801c9440e368a1fc444330307f92cd19f0f
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-07-09 08:07:23 -0400

    fear: add Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,6 @@
+import { ActivityIndicator } from "react-native";
+
+export const Loading = ()=>{
+    return ( <ActivityIndicator className='flex-1 bg-zinc-950' items-center justify-center text-lime-300/>)
+
+};
