d07652c08806ec4db7ff3941204f7365c9fd15e9
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-07-09 22:35:58 -0400

    feat: add logic to show hide input and make them editable or not, used enum

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { View, Image, Text } from 'react-native';
 import  { MapPin , Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from 'lucide-react-native';
 import { colors } from '@/styles/colors';
@@ -5,7 +6,21 @@ import { Input } from '@/components/Input';
 import { Button } from '@/components/Button';
 
 
+enum StepForm {
+    TRIP_DETAILS = 1,
+    ADD_EMAIL = 2,
+ }
+
+
 export const Index =()=>{
+    const [ stepForm, setStepForm ] = useState(StepForm.TRIP_DETAILS);
+
+
+    const handleNextStepForm = () =>{
+        if(stepForm === StepForm.TRIP_DETAILS){
+            return setStepForm(StepForm.ADD_EMAIL);
+        }
+    };
 
     return ( 
     <View className='flex-1 items-center justify-center px-5'>
@@ -23,28 +38,35 @@ export const Index =()=>{
         <View className='p-4 bg-zinc-900 rounded-xl my-8 w-full border border-zinc-800'>
             <Input >
                 <MapPin size={20} color={colors.zinc[400]}/>
-                <Input.Field  placeholder='Where are you going to ?'  />
+                <Input.Field editable={stepForm === StepForm.TRIP_DETAILS} placeholder='Where are you going to ?'  />
             </Input>
 
-            <Input >
+            <Input>
                 <IconCalendar size={20} color={colors.zinc[400]}/>
-                <Input.Field  placeholder='When ?'  />
+                <Input.Field editable={stepForm === StepForm.TRIP_DETAILS}  placeholder='When ?'  />
             </Input>
 
-            <View className='border-b py-3 border-zinc-800'>
-                <Button variant='secondary'>
-                  <Button.Title >Change place/date</Button.Title>
-                    <Settings2 size={20} color={colors.zinc[200]}/>
-                </Button>
-            </View>
+            { stepForm === StepForm.ADD_EMAIL &&
+            
+            <>    
+                <View className='border-b py-3 border-zinc-800'>
+                    <Button variant='secondary' onPress={()=>setStepForm(StepForm.TRIP_DETAILS)}>
+                    <Button.Title >Change place/date</Button.Title>
+                        <Settings2 size={20} color={colors.zinc[200]}/>
+                    </Button>
+                </View>
 
-            <Input >
-                <UserRoundPlus size={20} color={colors.zinc[400]}/>
-                <Input.Field  placeholder='Who is going with you ?'  />
-            </Input>
+                <Input >
+                    <UserRoundPlus size={20} color={colors.zinc[400]}/>
+                    <Input.Field  placeholder='Who is going with you ?'  />
+                </Input>
+            </>
+            }
+            <Button onPress={handleNextStepForm}>
+                  <Button.Title >
+                    { stepForm === StepForm.TRIP_DETAILS? 'Continue': 'Confirm Trip' }
+                    </Button.Title>
 
-            <Button>
-                  <Button.Title >Continue</Button.Title>
                     <ArrowRight size={20} color={colors.lime[950]}/>
             </Button>
 
