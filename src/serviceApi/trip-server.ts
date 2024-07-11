56e8cc3d3463d01b8ffa1f42acfe690277a94873
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-07-10 21:21:40 -0400

    feat: add createTrip function  POST

--- src/serviceApi/trip-server.ts
+++ src/serviceApi/trip-server.ts
@@ -9,17 +9,44 @@ export type TripDetails = {
 };
 
 
+type TripCreate = Omit<TripDetails, 'id' | 'is_confirmed'> & {
+    emails_to_invite: string[];
+};
+
 const getById = async(id: string)=>{
     try{
         const { data }  =  await api.get<{trip: TripDetails}>(`/trips/${id}`);
 
+        return data.trip;
 
     }catch(error){
         throw error;
     }
 };
 
+const createTrip = async({ destination, ends_at, starts_at, emails_to_invite }: TripCreate)=>{
+    // como ainda não tem autenticacao social (google por exemplo colocamos o nome e email fixo por enquanto)
+
+    try{
+        const { data }  =  await api.post<{tripId: string}>('/trips', {
+            destination,
+            ends_at,
+            starts_at,
+            emails_to_invite,
+            owner_name: 'Gilmara Pimentel',
+            owner_email: 'gilmarapq@hotmail.com'
+
+        });
+
+        
+        return data.tripId;
+
+
+    }catch(error){
+        throw error;
+    }
+}
 
 
 
-export  { getById }
+export const tripServer =  { getById , createTrip };
