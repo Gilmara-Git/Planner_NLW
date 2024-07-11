9aed6b5173fb03c899305bcd382e5eea1ecd7124
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-07-11 19:12:16 -0400

    feat: add utils functions

--- src/utils/dayjsLocaleConfig.ts
+++ src/utils/dayjsLocaleConfig.ts
@@ -1,5 +1,8 @@
-import "dayjs/locale/pt-br"
+// import "dayjs/locale/pt-br"
+import "dayjs/locale/en"
 import dayjs from "dayjs"
 
 // Para traduzir os nomes de dias e mês para pt-br.
-dayjs.locale("pt-br")
+// dayjs.locale("pt-br")
+
+dayjs.locale("en")
